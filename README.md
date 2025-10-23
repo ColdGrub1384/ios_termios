f99660ac6e3e72cbc4489b4e12f7f33eea6d19af
Emma Labbé <emma@labbe.me>
2025-10-23 19:50:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# ios_termios
+
+`ios_termios` is a wrapper of `tcgetattr`, `tcsetattr`, `tcgetwinsize` and `tcsetwinsize` for iOS. Because iOS doesn't let processes open pseudo terminals, this library manage their own. I'm trying to get `libedit` and the Python `readline` library to work with this wrapper.
+
+The client is still responsible of opening the pipes. This library will no open fake pseudo terminals for you but instead you register your file descriptors with the `ios_register_pty` function.
+
+## Usage
+
+To use this library, you must link this Swift Package or the framework compiled from the Xcode project to your app. This will make the necessary symbols available to the libraries that you want to compile.
+
+To compile a library using `termios`, you must include the `ios_termios.h` file either alongside `termios.h` or in place or `termios.h`. This will replace calls to termios functions with the appropriate wrapper.
+
+Before using this library, you must call `ios_register_pty` with any unique identifier, optionally, initial `termios` and `winsize` structures, followed by the file descriptors corresponding to the `stdin`, `stdout` and `stderr` streams managed by your terminal. If you don't do that, `termios` will raise `ENOTTY` and return `-1`.
+
+After the program finished using the pty, clear its attributes from memory with the `ios_clear_pty` function.
+
+## API
+
+All the APIs from this library are declared and thus usable in Swift and exposed to C.
+These are the only two functions of this library besides the termios wrappers:
+
+```c
+public func ios_register_pty(_ name: UnsafeMutableRawPointer, termp: UnsafeMutablePointer<termios>?, winp: UnsafeMutablePointer<winsize>?, stdin: Int32, stdout: Int32, stderr: Int32)
+
+public func ios_clear_pty(_ name: UnsafeMutableRawPointer)
+```
+
+Termios wrapper functions:
+
+```c
+
+int ios_tcgetwinsize(int fd, struct winsize *w);
+int ios_tcsetwinsize(int fd, const struct winsize *w);
+
+int ios_tcgetattr(int fd, struct termios *termios_p);
+int ios_tcsetattr(int fd, int optional_actions, const struct termios *termios_p);
+```
+
+And functions that do nothing and return 0:
+
+```c
+
+int ios_tcsendbreak(int fd, int duration);
+int ios_tcdrain(int fd);
+int ios_tcflush(int fd, int queue_selector);
+int ios_tcflow(int fd, int action);
+```
--- Sources/ios_termios/ios_termios.swift
+++ Sources/ios_termios/ios_termios.swift
@@ -1,2 +1,150 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+let queue = DispatchQueue.global(qos: .userInitiated)
+
+// Each termios and winsize is associated to a name, which is any type really
+nonisolated(unsafe) var _termios = [UnsafeMutableRawPointer:termios]()
+nonisolated(unsafe) var _winsize = [UnsafeMutableRawPointer:winsize]()
+
+// And each fake pty is associated to that same name
+nonisolated(unsafe) var ptys = [UnsafeMutableRawPointer:[Int32]]()
+
+// Default termios values
+var defaultTermios: termios {
+    var cc = [cc_t](repeating: 0, count: 20)
+    cc[Int(VINTR)]  = 3
+    cc[Int(VEOF)]   = 4
+    cc[Int(VERASE)] = 127
+    cc[Int(VKILL)]  = 21
+    cc[Int(VMIN)]   = 1
+    cc[Int(VTIME)]  = 0
+    return termios(
+        c_iflag: tcflag_t(BRKINT | ICRNL | IXON | IXANY | IMAXBEL),
+        c_oflag: tcflag_t(OPOST | ONLCR),
+        c_cflag: tcflag_t(CREAD | CS8 | HUPCL),
+        c_lflag: tcflag_t(ISIG | ICANON | ECHO | ECHOE | ECHOK),
+        c_cc: (
+            cc[0], cc[1], cc[2], cc[3], cc[4],
+            cc[5], cc[6], cc[7], cc[8], cc[9],
+            cc[10], cc[11], cc[12], cc[13], cc[14],
+            cc[15], cc[16], cc[17], cc[18], cc[19]
+        ),
+        c_ispeed: 9600,
+        c_ospeed: 9600
+    )
+}
+
+func getName(fd: Int32) -> UnsafeMutableRawPointer? {
+    ptys.filter({ $0.value.contains(fd) }).first?.key
+}
+
+// MARK: - API
+
+@_cdecl("ios_register_pty")
+public func ios_register_pty(_ name: UnsafeMutableRawPointer, termp: UnsafeMutablePointer<termios>?, winp: UnsafeMutablePointer<winsize>?, stdin: Int32, stdout: Int32, stderr: Int32) {
+
+    queue.sync {
+        ptys[name] = [stdin, stdout, stderr]
+        _termios[name] = termp?.pointee
+        _winsize[name] = winp?.pointee
+    }
+}
+
+@_cdecl("ios_clear_pty")
+public func ios_clear_pty(_ name: UnsafeMutableRawPointer) {
+    queue.sync {
+        ptys[name] = nil
+        _termios[name] = nil
+        _winsize[name] = nil
+    }
+}
+
+// MARK: - Wrappers
+
+@_cdecl("ios_tcsendbreak")
+public func ios_tcsendbreak(_ fd: Int32, _ duration: Int32) -> Int32 {
+    guard getName(fd: fd) != nil else {
+        errno = ENOTTY
+        return -1
+    }
+    return 0
+}
+
+@_cdecl("ios_tcdrain")
+public func ios_tcdrain(_ fd: Int32) -> Int32 {
+    guard getName(fd: fd) != nil else {
+        errno = ENOTTY
+        return -1
+    }
+    return 0
+}
+
+@_cdecl("ios_tcflush")
+public func ios_tcflush(_ fd: Int32, _ queue_selector: Int32) -> Int32 {
+    guard getName(fd: fd) != nil else {
+        errno = ENOTTY
+        return -1
+    }
+    return 0
+}
+
+@_cdecl("ios_tcflow")
+public func ios_tcflow(_ fd: Int32, _ action: Int32) -> Int32 {
+    guard getName(fd: fd) != nil else {
+        errno = ENOTTY
+        return -1
+    }
+    return 0
+}
+
+@_cdecl("ios_tcgetattr")
+public func ios_tcgetattr(_ fd: Int32, _ termios: UnsafeMutablePointer<termios>!) -> Int32 {
+
+    guard let name = getName(fd: fd) else {
+        errno = ENOTTY
+        return -1
+    }
+
+    termios.pointee = _termios[name] ?? defaultTermios
+    return 0
+}
+
+@_cdecl("ios_tcsetattr")
+public func ios_tcsetattr(_ fd: Int32, _ optional_actions: Int32, _ termios_p: UnsafeMutablePointer<termios>!) -> Int32 {
+
+    guard let name = getName(fd: fd) else {
+        errno = ENOTTY
+        return -1
+    }
+
+    queue.sync {
+        _termios[name] = termios_p.pointee
+    }
+    return 0
+}
+
+@_cdecl("ios_tcgetwinsize")
+public func ios_tcgetwinsize(_ fd: Int32, _ winsize_p: UnsafeMutablePointer<winsize>!) -> Int32 {
+
+    guard let name = getName(fd: fd) else {
+        errno = ENOTTY
+        return -1
+    }
+
+    winsize_p.pointee = _winsize[name] ?? winsize(ws_row: 0, ws_col: 0, ws_xpixel: 0, ws_ypixel: 0)
+    return 0
+}
+
+@_cdecl("ios_tcsetwinsize")
+public func ios_tcsetwinsize(_ fd: Int32, _ optional_actions: Int32, _ winsize_p: UnsafeMutablePointer<winsize>!) -> Int32 {
+
+    guard let name = getName(fd: fd) else {
+        errno = ENOTTY
+        return -1
+    }
+
+    queue.sync {
+        _winsize[name] = winsize_p.pointee
+    }
+    return 0
+}
